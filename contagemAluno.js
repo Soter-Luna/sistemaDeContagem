11cb467eb71c39134311f1e6a0264e1b6ef035ee
Soter-Luna <soter_luna@yahoo.com.br>
2022-01-27 09:57:08 -0300

    criando sistema de contagem e classificação

---
+++ contagemAluno.js
@@ -0,0 +1,26 @@
+let numeroAlunos = 10;
+
+for (let contador = 0; contador < numeroAlunos; contador++) {
+
+    if(contador == 0){
+        console.log("O numero é ZERO")
+    } else if(contador % 2 == 0){
+        console.log(`O numero ${contador} é PAR`)
+    } else if(contador % 2 == 1){
+        console.log(`O numero ${contador} é IMPAR`)
+    }
+}
+
+
+// let dataEvento = new Date("01/28/2022")
+// let dataHoje = new Date()
+
+
+// let listaParticipantes = ["Sóter", "Fabio", "Carlos"]
+
+// for (let contador = 0; contador < listaParticipantes.length; contador++){
+//     console.log(listaParticipantes[contador])
+// }
+
+
+
